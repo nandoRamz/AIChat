a401b62f39dfebf633702efcb3fc0e428af6c3e1
Nando <iframz13@gmail.com>
2025-01-28 06:44:26 -0800

    [Feature] Add app icon, note section

---
+++ AIChat/NOTES.swift
@@ -0,0 +1,8 @@
+//
+//  NOTES.swift
+//  AIChat
+//
+//  Created by Nando on 1/28/25.
+//
+
+//Creating a luanch screen: Create a launch screen from templates. In the info plist look for a launch screen property. There are 2 Launch Screen and Launch Screen interface file base name (use this one if using a story board.
